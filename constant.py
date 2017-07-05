6e201e1653a239d9c000905d228b607ae3593aba
rajneeshchaudhary <rajnishchaudhary786@gmail.com>
2017-07-05 12:19:58 +0530

    Now we start basic program......

---
+++ constant.py
@@ -0,0 +1,6 @@
+#In this file we declare the global variable
+APP_ACCESS_TOKEN = '2130880099.2a05397.ed706fff38dd45898b17b3e09a1ab787'
+
+
+#here is the base url of the instagram
+BASE_URL = 'https://api.instagram.com/v1/'
