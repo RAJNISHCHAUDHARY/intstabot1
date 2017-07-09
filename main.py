01f02e152ea566231454126285def29d6d760f85
rajneeshchaudhary <rajnishchaudhary786@gmail.com>
2017-07-09 13:55:20 +0530

    now we access the own recent post

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 #here is main file of instabot
 from self_info import self_info
+from get_own_post import get_own_post
 
 
 #fetching own information
-self_info()
+#self_info()
+
+
+#getching own recent post information
+result = get_own_post()
+print result
